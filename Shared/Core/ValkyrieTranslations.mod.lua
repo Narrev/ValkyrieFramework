ad50b2d222dedb1432afccd7f0aed1d7064d8bf9
GSKW <agoldstar101@gmail.com>
2015-11-04 16:33:42 +0200

    Translated all English. (I really had to do the tabs sorry)

--- Shared/Core/ValkyrieTranslations.mod.lua
+++ Shared/Core/ValkyrieTranslations.mod.lua
@@ -1,27 +1,30 @@
-local Translation = require(script.Parent.Components.Translation);
-local new = Translation.CreateNode;
-local translations = {};
+local Translation   = require(script.Parent.Components.Translation);
+local new           = Translation.CreateNode;
+local translations  = {};
 
 translations.GetComponent = new("Valkyrie.GetComponent", {
-    default = "GetComponent";
-    en_en = "GetComponent";
-    en_uk = "GetComponent";
-    en_us = "GetComponent";
-    eo_eo = "TrovuKomponanto";
+    default         = "GetComponent";
+    en_en           = "GetComponent";
+    en_uk           = "GetComponent";
+    en_us           = "GetComponent";
+    eo_eo           = "TrovuKomponanto";
+    fi_fi           = "HaeKomponentti";
 });
 translations.GetService = new("Valkyrie.GetService", {
-    default = "GetService";
-    en_en = "GetService";
-    en_uk = "GetService";
-    en_us = "GetService";
-    eo_eo = "TrovuRimedo"; -- Rimedo -> Resource
+    default         = "GetService";
+    en_en           = "GetService";
+    en_uk           = "GetService";
+    en_us           = "GetService";
+    eo_eo           = "TrovuRimedo"; -- Rimedo -> Resource
+    fi_fi           = "HaePalvelu";
 });
 translations.LoadLibrary = new("Valkyrie.LoadLibrary", {
-    default = "LoadLibrary";
-    en_en = "LoadLibrary";
-    en_uk = "LoadLibrary";
-    en_us = "LoadLibrary";
-    eo_eo = "SxarguLibrejo";
+    default         = "LoadLibrary";
+    en_en           = "LoadLibrary";
+    en_uk           = "LoadLibrary";
+    en_us           = "LoadLibrary";
+    eo_eo           = "SxarguLibrejo";
+    fi_fi           = "LataaKirjasto";
 });
 
 
