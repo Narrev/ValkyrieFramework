47233771e7ba87174c75793f3ad087048a9d4397
GSKW <agoldstar101@gmail.com>
2015-11-04 16:28:32 +0200

    Fixed ValkyrieTranslation.mod.lua indentation

--- Shared/Core/ValkyrieTranslations.mod.lua
+++ Shared/Core/ValkyrieTranslations.mod.lua
@@ -3,25 +3,25 @@ local new = Translation.CreateNode;
 local translations = {};
 
 translations.GetComponent = new("Valkyrie.GetComponent", {
-default = "GetComponent";
-en_en = "GetComponent";
-en_uk = "GetComponent";
-en_us = "GetComponent";
-eo_eo = "TrovuKomponanto";
+    default = "GetComponent";
+    en_en = "GetComponent";
+    en_uk = "GetComponent";
+    en_us = "GetComponent";
+    eo_eo = "TrovuKomponanto";
 });
 translations.GetService = new("Valkyrie.GetService", {
-default = "GetService";
-en_en = "GetService";
-en_uk = "GetService";
-en_us = "GetService";
-eo_eo = "TrovuRimedo"; -- Rimedo -> Resource
+    default = "GetService";
+    en_en = "GetService";
+    en_uk = "GetService";
+    en_us = "GetService";
+    eo_eo = "TrovuRimedo"; -- Rimedo -> Resource
 });
 translations.LoadLibrary = new("Valkyrie.LoadLibrary", {
-default = "LoadLibrary";
-en_en = "LoadLibrary";
-en_uk = "LoadLibrary";
-en_us = "LoadLibrary";
-eo_eo = "SxarguLibrejo";
+    default = "LoadLibrary";
+    en_en = "LoadLibrary";
+    en_uk = "LoadLibrary";
+    en_us = "LoadLibrary";
+    eo_eo = "SxarguLibrejo";
 });
 
 
