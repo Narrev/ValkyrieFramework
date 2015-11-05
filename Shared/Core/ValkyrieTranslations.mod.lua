8d38fef6e3c42fdf2d41d372232b966ed4e3bb56
eLunate <exp.lunate@gmail.com>
2015-11-05 11:02:00 +0000

    Update ValkyrieTranslations.mod.lua
    Provide additional translation targets and translations

--- Shared/Core/ValkyrieTranslations.mod.lua
+++ Shared/Core/ValkyrieTranslations.mod.lua
@@ -7,7 +7,7 @@ translations.GetComponent = new("Valkyrie.GetComponent", {
     en_en           = "GetComponent";
     en_uk           = "GetComponent";
     en_us           = "GetComponent";
-    eo_eo           = "TrovuKomponanto";
+    eo_eo           = "TrovuKomponanton";
     fi_fi           = "HaeKomponentti";
 });
 translations.GetService = new("Valkyrie.GetService", {
@@ -15,7 +15,7 @@ translations.GetService = new("Valkyrie.GetService", {
     en_en           = "GetService";
     en_uk           = "GetService";
     en_us           = "GetService";
-    eo_eo           = "TrovuRimedo"; -- Rimedo -> Resource
+    eo_eo           = "TrovuRimedon"; -- Rimedo -> Resource
     fi_fi           = "HaePalvelu";
 });
 translations.LoadLibrary = new("Valkyrie.LoadLibrary", {
@@ -23,7 +23,7 @@ translations.LoadLibrary = new("Valkyrie.LoadLibrary", {
     en_en           = "LoadLibrary";
     en_uk           = "LoadLibrary";
     en_us           = "LoadLibrary";
-    eo_eo           = "SxarguLibrejo";
+    eo_eo           = "SxarguLibrejon";
     fi_fi           = "LataaKirjasto";
 });
 translations.SetComponent = new("Valkyrie.SetComponent", {
@@ -31,7 +31,7 @@ translations.SetComponent = new("Valkyrie.SetComponent", {
     en_en           = "SetComponent";
     en_uk           = "SetComponent";
     en_us           = "SetComponent";
-    eo_eo           = "...";
+    eo_eo           = "LokuKomponanton";
     fi_fi           = "AsetaKomponentti";
 });
 translations.SetService = new("Valkyrie.SetService", {
@@ -39,7 +39,7 @@ translations.SetService = new("Valkyrie.SetService", {
     en_en           = "SetService";
     en_uk           = "SetService";
     en_us           = "SetService";
-    eo_eo           = "...";
+    eo_eo           = "LokuRimedon";
     fi_fi           = "AsetaPalvelu";
 });
 translations.GetSettings = new("Valkyrie.GetSettings", {
@@ -47,9 +47,36 @@ translations.GetSettings = new("Valkyrie.GetSettings", {
     en_en           = "GetSettings";
     en_uk           = "GetSettings";
     en_us           = "GetSettings";
-    eo_eo           = "...";
+    eo_eo           = "TrovuAgordojn";
     fi_fi           = "HaeAsetukset";
 });
-
+translations.GetGID = new("Valkyrie.GetGID", {
+	default = "GetGID";
+	en_en = "GetGID";
+	en_uk = "GetGID";
+	en_us = "GetGID";
+	eo_eo = "TrovuLID";
+});
+translations.GetURL = new("Valkyire.GetURL", {
+	default = "GetURL";
+	en_en = "GetURL";
+	en_uk = "GetURL";
+	en_us = "GetURL";
+	eo_eo = "TrovuURL";
+});
+translations.AddLibrary = new("Valkyrie.AddLibrary", {
+	default = "AddLibrary";
+	en_en = "AddLibrary";
+	en_uk = "AddLibrary";
+	en_us = "AddLibrary";
+	eo_eo = "AldonuLibrejon";
+});
+translations.GenerateWrapper = new("Valkyrie.GenerateWrapper", {
+	default = "GenerateWrapper";
+	en_en = "GenerateWrapper";
+	en_uk = "GenerateWrapper";
+	en_us = "GenerateWrapper";
+	eo_eo = "EstiguProkurilon";
+});
 
 return translations;
