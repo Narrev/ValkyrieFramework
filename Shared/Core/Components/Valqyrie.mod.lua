c9271ce41151751000d6cd0a52eb0c6550c1f5a9
eLunate <exp.lunate@gmail.com>
2016-01-21 10:49:54 +0000

    Update Valqyrie.mod.lua
    Fill in some points in the metatables.

--- Shared/Core/Components/Valqyrie.mod.lua
+++ Shared/Core/Components/Valqyrie.mod.lua
@@ -4,15 +4,36 @@ local pack = function(...)
 	return {n=select('#',...),...};
 end;
 
-local rechainer = setmetatable({},{__mode='k'});
+local rechainer = setmetatable({},{__mode=''k'}),setmetatable({},{__mode='k'});
 local qyrobjmt,qyrentrymt,qyrlist,qyrimmediate,qyrimmediatemt;
 local qyrobjmtp,qyrentrymtp,qyrimmediatemtp = newproxy(false),newproxy(false),newproxy(false);
 qyrobjmt = {
 	__call = function(t,...)
 		local this = rechainer[t];
 		-- Assumes that the rechainer actually kept the reference.
-		local r = pack(qyrlist[this.chainnext](this,...));
-		return qyrimmediate(this,unpack(r,1,r.n));
+		return qyrimmediate(this,qyrlist[this.chainnext](this,...));
 	end;
-	__metatable = 
-}
+	__metatable = qyrobjmtp;
+};
+qyrimmediatemt = {
+	__index = function(t,k)
+		
+	end;
+	__newindex = function(t,k,v)
+		
+	end;
+	__metatable = qyrimmediatemtp;
+};
+qyrentrymt = {
+	__call = function(t,s)
+		
+		return qyrimmediate(selection);
+	end;
+	__metatable = qyrentrymtp;
+};
+
+	
+function qyrimmediate(chain, ...)
+	-- Return the Immediate object. This stores information about the current chain.
+	
+end;
